7c479f19b6c803ac9ab7cda6b303b17d32ff1be2
Hardik-4994 <hardikgohel2452004@gmail.com>
2022-10-17 09:21:11 +0530

    init

--- 15-10.py
+++ 15-10.py
@@ -8,6 +8,5 @@ listSample = Listbox(root , width=15,height=30 , fg="red" , font=(" Arial", 28))
 listSample.insert(1, "Margerita")
 listSample.insert(2,"Thin Crust")
 listSample.insert(3,"Cheese Burst")
-
 listSample.pack()
 root.mainloop()
